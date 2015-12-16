d619b73f9738536a7380ac371b0c1fbc27a25d3b
earrivi-ibit <earrivi-ibit@2f747da4-73e4-43c8-ac1f-8a61286d5fbb>
2015-12-16 08:48:58 +0000

    Modificado el tamaño del campo razón social para Interesado
    git-svn-id: https://svn.code.sf.net/p/registre/code/registre-core/tags/registre-3.0.3@559 2f747da4-73e4-43c8-ac1f-8a61286d5fbb

--- model/src/main/java/es/caib/regweb3/model/Interesado.java
+++ model/src/main/java/es/caib/regweb3/model/Interesado.java
@@ -296,7 +296,7 @@ public class Interesado implements Serializable {
         this.tipo = tipo;
     }
 
-    @Column(name = "RAZONSOCIAL", length = 80)
+    @Column(name = "RAZONSOCIAL", length = 2000) // Modificado a 2000 para permitir las Administraciones
     public String getRazonSocial() {
         return razonSocial;
     }
---
+++ scripts/bbdd/3.0/postgresql/regweb3_update_schema_from_3.0.2_to-3.0.3.sql
@@ -0,0 +1,2 @@
+--Aumento tamaño del campo Razón Social
+alter table RWE_INTERESADO alter RAZONSOCIAL type varchar(2000);
