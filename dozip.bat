1c4cce8aa75e0305548d5da10b7006eb98af91b8
anadal-ibit <anadal-ibit@2f747da4-73e4-43c8-ac1f-8a61286d5fbb>
2016-02-19 11:09:12 +0000

    Millorar generador de releases.
    git-svn-id: https://svn.code.sf.net/p/registre/code/registre-core/branches/registre-3.0@632 2f747da4-73e4-43c8-ac1f-8a61286d5fbb

--- dozip.bat
+++ dozip.bat
@@ -1,4 +1,4 @@
 
 for /f "tokens=* delims=" %%x in (versio.txt) do set REGWEB_VERSIO=%%x
 
-zip -r registre%1-%REGWEB_VERSIO%.zip scripts doc/pdf versio.txt ear/target/regweb3.ear ws/regweb3_api/target/regweb3-ws-api-*.jar ws/regweb3_api_axis/target/regweb3-ws-api-axis-*.jar ws/sir_api/target/regweb3-ws-sir-api-*.jar -x "**/.svn**"  -x "scripts/sqlgenenerator/**" -x "scripts/pom.xml"
+zip -r   registre%1-%REGWEB_VERSIO%.zip scripts doc/pdf versio.txt ear/target/regweb3.ear ws/regweb3_api/target/regweb3-ws-api-*.jar ws/regweb3_api_axis/target/regweb3-ws-api-axis-*.jar ws/sir_api/target/regweb3-ws-sir-api-*.jar -x "**/.svn**"  -x "./scripts/sqlgenerator/**" -x "./scripts/pom.xml" -x "./scripts/templates/**" -x "./scripts/sqlgenerator/" -x "./scripts/templates/"
