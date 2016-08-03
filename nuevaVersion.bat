e58e645a7905f3e5d78272cdd2dfe1ff3a5ae008
earrivi <earrivi@fundaciobit.org>
2016-08-03 12:00:52 +0200

    Mejoras en el scrip para versionar la aplicación, ahora lo hace en todos los modulos de la aplicación.

--- nuevaVersion.bat
+++ nuevaVersion.bat
@@ -1,12 +1,2 @@
 
-cmd /C mvn versions:set -DnewVersion=%*
-
-
-@echo.
-@echo.
-@echo --------------------------- IMPORTANT ----------------------------
-@echo ^|  El projectes del directori ws no s'actualitzen automaticament. ^|
-@echo ^|  Per favor actualitzar la versio manualment.                    ^|
-@echo -------------------------------------------------------------------
-@echo.
-@echo.
+cmd /C mvn -DgroupId=es.caib.regweb3 -DartifactId=* versions:set -DnewVersion=%*
--- nuevaVersion.sh
+++ nuevaVersion.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
+env mvn -DgroupId=es.caib.regweb3 -DartifactId=* versions:set -DnewVersion=$@
 
-env mvn versions:set -DnewVersion=$@
-
-
-echo -e "\n"
-echo -e "\n"
-echo --------------------------- IMPORTANT ------------------------------
-echo "|  El projectes del directori ws no s\'actualitzen automaticament. |"
-echo "|  Per favor actualitzar la versio manualment.                     |"
-echo --------------------------------------------------------------------
-echo -e "\n"
-echo -e "\n"
