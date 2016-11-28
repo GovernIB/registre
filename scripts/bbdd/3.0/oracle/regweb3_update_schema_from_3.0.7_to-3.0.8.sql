1e4db6b57e5153b17ee13698ba6d01e8018394b9
earrivi <earrivi@fundaciobit.org>
2016-11-28 11:49:30 +0100

    PropiedadGlobal: Resultados por página de diferentes informes se gestionan mediante una propiedad.

--- scripts/bbdd/3.0/oracle/regweb3_update_schema_from_3.0.7_to-3.0.8.sql
+++ scripts/bbdd/3.0/oracle/regweb3_update_schema_from_3.0.7_to-3.0.8.sql
@@ -23,6 +23,6 @@ ALTER TABLE RWE_TRAZABILIDAD MODIFY REGENT_ORIGEN NULL;
 alter table RWE_ENTIDAD add (OFICIOREMISION NUMBER(1) DEFAULT 1 NOT NULL);
 
 --Contenido para cada Entidad de la tabla RWE_PROPIEDADGLOBAL (Es necesario especificar el id de la Entidad a la que pertenecen)
-INSERT INTO RWE_PROPIEDADGLOBAL (id,clave,valor,tipo,descripcion,entidad) VALUES (RWE_ALL_SEQ.nextVal,'es.caib.regweb3.resultsperpage','10',1,'Resultados por página en los listados paginados',12264);
-INSERT INTO RWE_PROPIEDADGLOBAL (id,clave,valor,tipo,descripcion,entidad) VALUES (RWE_ALL_SEQ.nextVal,'es.caib.regweb3.resultsperpage.oficios','20',1,'Resultados por página en los Oficios pendientes de remisión',12264);
-INSERT INTO RWE_PROPIEDADGLOBAL (id,clave,valor,tipo,descripcion,entidad) VALUES (RWE_ALL_SEQ.nextVal,'es.caib.regweb3.resultsperpage.lopd','20',1,'Resultados por página en los informes LOPD',12264);
+INSERT INTO RWE_PROPIEDADGLOBAL (id,clave,valor,tipo,descripcion,entidad) VALUES (RWE_ALL_SEQ.nextVal,'es.caib.regweb3.resultsperpage','10',1,'Resultados por página en los listados paginados',?);
+INSERT INTO RWE_PROPIEDADGLOBAL (id,clave,valor,tipo,descripcion,entidad) VALUES (RWE_ALL_SEQ.nextVal,'es.caib.regweb3.resultsperpage.oficios','20',1,'Resultados por página en los Oficios pendientes de remisión',?);
+INSERT INTO RWE_PROPIEDADGLOBAL (id,clave,valor,tipo,descripcion,entidad) VALUES (RWE_ALL_SEQ.nextVal,'es.caib.regweb3.resultsperpage.lopd','20',1,'Resultados por página en los informes LOPD',?);
