d0dc3bc84e6d62816b1190c5ca7106dbc98dd9fe
earrivi <earrivi@fundaciobit.org>
2016-12-19 12:32:32 +0100

    Resuelto bug: Id duplicado en formulario cuando existian más de un modelo de recibo

--- webapp/src/main/java/es/caib/regweb3/webapp/controller/registro/RegistroEntradaListController.java
+++ webapp/src/main/java/es/caib/regweb3/webapp/controller/registro/RegistroEntradaListController.java
@@ -11,6 +11,7 @@ import es.caib.regweb3.persistence.utils.RegistroUtils;
 import es.caib.regweb3.persistence.utils.RespuestaDistribucion;
 import es.caib.regweb3.utils.RegwebConstantes;
 import es.caib.regweb3.utils.StringUtils;
+import es.caib.regweb3.webapp.form.ModeloForm;
 import es.caib.regweb3.webapp.form.RegistroEntradaBusqueda;
 import es.caib.regweb3.webapp.utils.Mensaje;
 import es.caib.regweb3.webapp.validator.RegistroEntradaBusquedaValidator;
@@ -201,7 +202,7 @@ public class RegistroEntradaListController extends AbstractRegistroCommonListCon
         model.addAttribute("oficina", oficinaActiva);
 
         // Modelo Recibo
-        model.addAttribute("modeloRecibo", new ModeloRecibo());
+        model.addAttribute("modeloRecibo", new ModeloForm());
         model.addAttribute("modelosRecibo", modeloReciboEjb.getByEntidad(entidadActiva.getId()));
 
         // Permisos
---
+++ webapp/src/main/java/es/caib/regweb3/webapp/form/ModeloForm.java
@@ -0,0 +1,23 @@
+package es.caib.regweb3.webapp.form;
+
+
+/**
+ * Created by Fundació Bit
+ * @author earrivi
+ */
+public class ModeloForm {
+
+    private Long idModelo;
+
+
+    public ModeloForm() {
+    }
+
+    public Long getIdModelo() {
+        return idModelo;
+    }
+
+    public void setIdModelo(Long idModelo) {
+        this.idModelo = idModelo;
+    }
+}
