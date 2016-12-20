30c8ee9afbed4f0c7161ff5279cf374074b821cf
earrivi <earrivi@fundaciobit.org>
2016-12-20 10:33:22 +0100

    Mejoras en las Repro

---
+++ webapp/src/main/java/es/caib/regweb3/webapp/form/ReproForm.java
@@ -0,0 +1,52 @@
+package es.caib.regweb3.webapp.form;
+
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by Fundació Bit
+ * @author earrivi
+ */
+@XmlRootElement(name = "reproForm")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ReproForm {
+
+    @XmlElement
+    private Long idRegistro;
+    @XmlElement
+    private Integer tipoRegistro;
+    @XmlElement
+    private String nombreRepro;
+
+
+
+    public ReproForm() {
+    }
+
+    public Long getIdRegistro() {
+        return idRegistro;
+    }
+
+    public void setIdRegistro(Long idRegistro) {
+        this.idRegistro = idRegistro;
+    }
+
+    public Integer getTipoRegistro() {
+        return tipoRegistro;
+    }
+
+    public void setTipoRegistro(Integer tipoRegistro) {
+        this.tipoRegistro = tipoRegistro;
+    }
+
+    public String getNombreRepro() {
+        return nombreRepro;
+    }
+
+    public void setNombreRepro(String nombreRepro) {
+        this.nombreRepro = nombreRepro;
+    }
+}
