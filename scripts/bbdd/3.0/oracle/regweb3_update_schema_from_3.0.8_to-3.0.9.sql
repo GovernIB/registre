fd72f51b0bcc62dd9243d74d164c128b6ce2f3cc
earrivi <earrivi@fundaciobit.org>
2017-03-06 13:19:06 +0100

    Cambio de versión a 3.0.9

---
+++ scripts/bbdd/3.0/oracle/regweb3_update_schema_from_3.0.8_to-3.0.9.sql
@@ -0,0 +1,5 @@
+--Eliminar campo de la tabla RWE_ASIENTO_REGISTRAL_SIR
+ALTER TABLE RWE_ASIENTO_REGISTRAL_SIR DROP COLUMN COD_ENT_REG;
+
+--Actualizar el nombre de la aplicación para adaptarlo a SICRES3
+update RWE_REGISTRO_DETALLE set APLICACION='RWE3';
