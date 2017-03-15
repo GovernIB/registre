d3d73e52ed67bbd99f4dac1ceb05704fcd7db81e
earrivi <earrivi@fundaciobit.org>
2017-03-15 13:47:44 +0100

    Cambios en create schema

--- scripts/bbdd/3.0/oracle/regweb3_create_schema.sql
+++ scripts/bbdd/3.0/oracle/regweb3_create_schema.sql
@@ -48,7 +48,6 @@
         ID number(19,0) not null,
         APLICACION varchar2(4 char),
         COD_ASUNTO varchar2(16 char),
-        COD_ENT_REG varchar2(21 char),
         COD_ENT_REG_DEST varchar2(21 char) not null,
         COD_ENT_REG_INI varchar2(21 char) not null,
         COD_ENT_REG_ORI varchar2(21 char) not null,
