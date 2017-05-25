f334fad320dc42c228602afd41586fbd77beb5d2
earrivi <earrivi@fundaciobit.org>
2017-05-25 13:34:43 +0200

    SIR: Descomentar validación carácteres extraños en el título de un Anexo recibido

--- sir/sir-api/src/main/java/es/caib/regweb3/sir/utils/Sicres3XML.java
+++ sir/sir-api/src/main/java/es/caib/regweb3/sir/utils/Sicres3XML.java
@@ -417,8 +417,8 @@ public class Sicres3XML {
             // Validar el nombre del fichero anexado
             Assert.hasText(anexo.getNombre_Fichero_Anexado(),
                     "El campo 'NombreFicheroAnexado' no puede estar vacio");
-            /*Assert.isTrue(!StringUtils.containsAny(anexo.getNombre_Fichero_Anexado(), "\\/?*:|<>\";&"),
-                    "El campo 'NombreFicheroAnexado' tiene caracteres no válidos [" + anexo.getNombre_Fichero_Anexado() + "]");*/
+            Assert.isTrue(!StringUtils.containsAny(anexo.getNombre_Fichero_Anexado(), "\\/?*:|<>\";&"),
+                    "El campo 'NombreFicheroAnexado' tiene caracteres no validos [" + anexo.getNombre_Fichero_Anexado() + "]");
 
             // Validar el identificador de fichero
             validarIdentificadorFichero(anexo, identificadorIntercambio);
