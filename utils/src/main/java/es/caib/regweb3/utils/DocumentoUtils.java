d72db7c561719d2d3b6654f55451a8ae3ae7a994
jpernia <jpernia@fundaciobit.org>
2018-08-30 09:18:48 +0200

    Arreglada la comprovació de NIF's començats per K, M, L

--- utils/src/main/java/es/caib/regweb3/utils/DocumentoUtils.java
+++ utils/src/main/java/es/caib/regweb3/utils/DocumentoUtils.java
@@ -24,6 +24,7 @@ public class DocumentoUtils {
     private static final String PATRON_CIF = "([abcdefghjklmnpqrsuvwABCDEFGHJKLMNPQRSUVW]{1})([0-9]{7})([0-9a-jA-J]{1})";
     private static final String PATRON_NIF = "([0-9]{8})([a-zA-Z]{1})";
     private static final String PATRON_NIE = "([xyzXYZ]{1})([0-9]{7})([a-zA-Z]{1})";
+    private static final String PATRON_NIF_NOU = "([klmKLMN]{1})([0-9]{7})([0-9a-zA-Z]{1})";
 
 
     /**
@@ -43,61 +44,30 @@ public class DocumentoUtils {
                 // Si el document comença amb K, L, M
                 if(documento.startsWith("k") || documento.startsWith("K") || documento.startsWith("m") || documento.startsWith("M") || documento.startsWith("l") || documento.startsWith("L")) {
 
-                    int codigo_de_control;
-                    Pattern pCif = Pattern.compile(PATRON_CIF);
+                    int valorNumerico;
+                    Pattern pCif = Pattern.compile(PATRON_NIF_NOU);
                     Matcher mCif = pCif.matcher(documento);
 
                     try {
 
                         if (mCif.matches()) { // Comprueba que el documento tiene el patrón correcto (LETRA + 7 DIGITOS + LETRA/NUMERO)
 
-                            // Obtiene la primera letra del CIF
-                            char tipoOrganizacion = documento.charAt(0);
-
-                            // Comprobar el código de control
-                            if (Character.isDigit(documento.charAt(8))) { // Si el último carácter es un número
-                                codigo_de_control = Character.getNumericValue(documento.charAt(8));  // Codigo de control es el último número
-
-                                if (codigo_de_control == 0) { // Si código de control es 0, le pone el valor 10
-                                    codigo_de_control = 10;
-                                }
-
-                                if (TIPO_ORGANIZACION_LETRA_CIF.indexOf(tipoOrganizacion) >= 0) {
-                                    codigo_de_control = 100;
-                                }
-                            } else {  // El último carácter es una letra
-                                codigo_de_control = CODIGO_CONTROL_CIF.indexOf(documento.charAt(8)) + 1; // Codigo de control es el índice de la letra
-
-                                if (TIPO_ORGANIZACION_NUMERO_CIF.indexOf(tipoOrganizacion) >= 0) {
-                                    codigo_de_control = 100;
-                                }
-                            }
-
-                            // Calcula A (valores de los pares) y B (suma de los dígitos de las posiciones impares *2)
-                            int a = 0, b = 0, c;
-                            byte[] impares = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
-
-                            for (int t = 1; t <= 6; t = t + 2) { // Recorre los números del CIF para calcular A y B
-                                a = a + Character.getNumericValue(documento.charAt(t + 1)); // Suma las posiciones pares de la parte numérica (2,4,6)
-                                b = b + impares[Character.getNumericValue(documento.charAt(t))];  // Suma las posiciones de los impares dentro de impares[]
-                            }
-                            b = b + impares[Character.getNumericValue(documento.charAt(7))];  // Añade el último dígito impar a B
-
-                            // Calcula C (para compararlo con el código de control)
-                            c = 10 - ((a + b) % 10);
-
-                            // Compara C con el dígito de control
-                            if (c != codigo_de_control) {  // Si no coincide, es erróneo
-                                return new Validacion(Boolean.FALSE, "error.documento.numero", "El número de nif és incorrecte");
-                            }
+                            String numeroNie = documento.substring(1, documento.length() - 1); // Suprimimos la letra inicial y final
+                            valorNumerico = Integer.parseInt(numeroNie); // Lo transformamos en un Integer
 
                         } else {
-                            return new Validacion(Boolean.FALSE, "error.dni.incorrecto", "El NIF no té format correcte (LLETRA + 7 NÚMEROS + LLETRA/NÚMERO)");
+                            return new Validacion(Boolean.FALSE, "error.dni.incorrecto2", "El NIF no és correcte");
+                        }
+
+                        //Comprueba que la letra final sea correcta
+                        if (!documento.endsWith("" + LETRAS_NIF.charAt(valorNumerico % 23))) {
+
+                            return new Validacion(Boolean.FALSE, "error.dni.incorrecto2", "El NIF no és correcte");
                         }
 
                     } catch (Exception e) {
                         e.printStackTrace();
-                        return new Validacion(Boolean.FALSE, "error.dni.incorrecto", "El NIF no té format correcte (LLETRA + 7 NÚMEROS + LLETRA/NÚMERO)");
+                        return new Validacion(Boolean.FALSE, "error.dni.incorrecto2", "El NIF no és correcte");
                     }
 
 
