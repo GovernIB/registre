dfb92ef04a1b6a54f49cd7fceb0d21499613ce84
earrivi <earrivi@fundaciobit.org>
2018-09-17 09:22:07 +0200

    Integraciones: Nueva pantalla que agrupa toda la información generada por un registro

---
+++ webapp/src/main/java/es/caib/regweb3/webapp/form/BasicForm.java
@@ -0,0 +1,23 @@
+package es.caib.regweb3.webapp.form;
+
+
+/**
+ * Created by Fundació Bit
+ * @author earrivi
+ */
+public class BasicForm {
+
+    private String texto;
+
+
+    public BasicForm() {
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
