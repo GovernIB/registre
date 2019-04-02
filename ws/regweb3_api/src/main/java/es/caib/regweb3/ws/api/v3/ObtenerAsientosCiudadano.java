8ec33d749307a2cc72d8901d889d3916f2414a58
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2019-04-02 13:29:12 +0200

    Api actualizada a la nueva capa de servicios AsientoRegistralWs

---
+++ ws/regweb3_api/src/main/java/es/caib/regweb3/ws/api/v3/ObtenerAsientosCiudadano.java
@@ -0,0 +1,87 @@
+
+package es.caib.regweb3.ws.api.v3;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para obtenerAsientosCiudadano complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="obtenerAsientosCiudadano">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="entidad" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="documento" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "obtenerAsientosCiudadano", propOrder = {
+    "entidad",
+    "documento"
+})
+public class ObtenerAsientosCiudadano {
+
+    protected String entidad;
+    protected String documento;
+
+    /**
+     * Obtiene el valor de la propiedad entidad.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEntidad() {
+        return entidad;
+    }
+
+    /**
+     * Define el valor de la propiedad entidad.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEntidad(String value) {
+        this.entidad = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad documento.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDocumento() {
+        return documento;
+    }
+
+    /**
+     * Define el valor de la propiedad documento.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDocumento(String value) {
+        this.documento = value;
+    }
+
+}
