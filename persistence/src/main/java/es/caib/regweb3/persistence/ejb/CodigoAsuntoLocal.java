514076c9fecfd916efa300658d12a010da736017
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2019-07-30 14:02:26 +0200

    Eliminado metodo que no se utiliza

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/CodigoAsuntoLocal.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/CodigoAsuntoLocal.java
@@ -74,11 +74,4 @@ public interface CodigoAsuntoLocal extends BaseEjb<CodigoAsunto, Long> {
      */
     Integer eliminarByEntidad(Long idEntidad) throws Exception;
 
-    /**
-     * Método de utilidad que asigna la entidad de un tipoAsunto a un código Asunto.
-     * Refactorización : Eliminación del tipoAsunto.
-     * @throws Exception
-     */
-    void actualizarEntidadCodigosAsunto() throws Exception;
-
 }
