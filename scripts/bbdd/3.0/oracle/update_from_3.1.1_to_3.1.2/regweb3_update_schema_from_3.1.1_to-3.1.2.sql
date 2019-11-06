282a06a5a1edcb8f05e7bc7b9cb915e43c20e456
earrivi <earrivi@fundaciobit.org>
2019-11-06 11:29:40 +0100

    WS: Nuevo sistema para crearAsientoRegistral usando un idSesion

--- scripts/bbdd/3.0/oracle/update_from_3.1.1_to_3.1.2/regweb3_update_schema_from_3.1.1_to-3.1.2.sql
+++ scripts/bbdd/3.0/oracle/update_from_3.1.1_to_3.1.2/regweb3_update_schema_from_3.1.1_to-3.1.2.sql
@@ -1,4 +1,4 @@
---Nuevo Table RWE_SESION
+--Nueva Tabla RWE_SESION
 create table RWE_SESION (
         ID number(19,0) not null,
         ESTADO number(19,0),
@@ -10,6 +10,7 @@ create table RWE_SESION (
     );
 
 create sequence RWE_SESION_SEQ;
+grant select on RWE_ALL_SEQ to www_regweb;
 create index RWE_SESION_USUENT_FK_I on RWE_SESION (USUARIO);
 alter table RWE_SESION add constraint RWE_SESION_pk primary key (ID);
 alter table RWE_SESION add constraint RWE_SESION_USUENT_FK foreign key (USUARIO) references RWE_USUARIO_ENTIDAD;
