c632775d66bba20b2e9c05ae6b8f3261b2694d13
jpernia <jpernia@fundaciobit.org>
2019-11-07 10:22:53 +0100

    -Añadido Rol de acceso "RWE_WS_CIUDADANO"

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/TrazabilidadLocal.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/TrazabilidadLocal.java
@@ -17,7 +17,7 @@ import java.util.Set;
  * Date: 16/01/14
  */
 @Local
-@RolesAllowed({"RWE_SUPERADMIN","RWE_ADMIN","RWE_USUARI"})
+@RolesAllowed({"RWE_SUPERADMIN","RWE_ADMIN","RWE_USUARI","RWE_WS_ENTRADA","RWE_WS_SALIDA", "RWE_WS_CIUDADANO"})
 public interface TrazabilidadLocal extends BaseEjb<Trazabilidad, Long> {
 
     /**
