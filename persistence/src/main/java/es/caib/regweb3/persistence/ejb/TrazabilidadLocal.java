e023eda0e389040735d8251ca5bc998c65b0caa0
earrivi <earrivi@fundaciobit.org>
2020-01-14 11:25:08 +0100

    Bug trazabilidades: Resolución bug en las trazabilidades al aceptar un oficio de remisión

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/TrazabilidadLocal.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/TrazabilidadLocal.java
@@ -19,7 +19,7 @@ import java.util.List;
 @RolesAllowed({"RWE_SUPERADMIN","RWE_ADMIN","RWE_USUARI","RWE_WS_ENTRADA","RWE_WS_SALIDA", "RWE_WS_CIUDADANO"})
 public interface TrazabilidadLocal extends BaseEjb<Trazabilidad, Long> {
 
-    List<Trazabilidad> oficiosSinREDestino() throws Exception;
+    List<Trazabilidad> oficiosSinREDestino(Long tipoOficio) throws Exception;
 
 
     /**
