c8db09ad060b35d268d3d50ad756ff0b3f0e1188
earrivi <earrivi@fundaciobit.org>
2020-02-19 11:52:55 +0100

    Sql: scripts de actualización de schema

--- scripts/bbdd/3.0/oracle/update_from_3.1.2_to_3.1.3/regweb3_update_schema_from_3.1.2_to-3.1.3_caib.sql
+++ scripts/bbdd/3.0/oracle/update_from_3.1.2_to_3.1.3/regweb3_update_schema_from_3.1.2_to-3.1.3_caib.sql
@@ -5,6 +5,9 @@ ALTER TABLE RWE_REGISTRO_DETALLE RENAME COLUMN solicita TO solicita2;
 ALTER TABLE RWE_REGISTRO_DETALLE ADD (expone CLOB);
 ALTER TABLE RWE_REGISTRO_DETALLE ADD (solicita CLOB);
 
+alter table RWE_REGISTRO_DETALLE move lob (expone) store as RWE_REG_DET_EXPONE_lob (tablespace regweb_lob index RWE_REG_DET_EXPONE_lob_i);
+alter table RWE_REGISTRO_DETALLE move lob (solicita) store as RWE_REG_DET_SOLICITA_lob (tablespace regweb_lob index RWE_REG_DET_SOLICITA_lob_i);
+
 UPDATE RWE_REGISTRO_DETALLE SET expone=expone2;
 UPDATE RWE_REGISTRO_DETALLE SET solicita=solicita2;
 ALTER TABLE RWE_REGISTRO_DETALLE DROP COLUMN expone2;
@@ -18,6 +21,9 @@ ALTER TABLE RWE_REGISTRO_SIR RENAME COLUMN solicita TO solicita2;
 
 ALTER TABLE RWE_REGISTRO_SIR ADD (expone CLOB);
 ALTER TABLE RWE_REGISTRO_SIR ADD (solicita CLOB);
+
+alter table RWE_REGISTRO_SIR move lob (expone) store as RWE_REG_SIR_EXPONE_lob (tablespace regweb_lob index RWE_REG_SIR_EXPONE_lob_i);
+alter table RWE_REGISTRO_SIR move lob (solicita) store as RWE_REG_SIR_SOLICITA_lob (tablespace regweb_lob index RWE_REG_SIR_SOLICITA_lob_i);
 
 UPDATE RWE_REGISTRO_SIR SET expone=expone2;
 UPDATE RWE_REGISTRO_SIR SET solicita=solicita2;
