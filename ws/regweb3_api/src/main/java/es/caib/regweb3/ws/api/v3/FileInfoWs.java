7dd7589988b464c459163a936c8db88253fbcf4d
earrivi <earrivi@fundaciobit.org>
2021-02-26 09:12:35 +0100

    Regenerar ws api

--- ws/regweb3_api/src/main/java/es/caib/regweb3/ws/api/v3/FileInfoWs.java
+++ ws/regweb3_api/src/main/java/es/caib/regweb3/ws/api/v3/FileInfoWs.java
@@ -17,6 +17,7 @@ import javax.xml.bind.annotation.XmlType;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element name="fileID" type="{http://www.w3.org/2001/XMLSchema}long" minOccurs="0"/>
+ *         &lt;element name="filename" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="mime" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="size" type="{http://www.w3.org/2001/XMLSchema}long" minOccurs="0"/>
@@ -31,6 +32,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "fileInfoWs", propOrder = {
     "fileID",
+    "filename",
     "mime",
     "name",
     "size"
@@ -38,6 +40,7 @@ import javax.xml.bind.annotation.XmlType;
 public class FileInfoWs {
 
     protected Long fileID;
+    protected String filename;
     protected String mime;
     protected String name;
     protected Long size;
@@ -64,6 +67,30 @@ public class FileInfoWs {
      */
     public void setFileID(Long value) {
         this.fileID = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad filename.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFilename() {
+        return filename;
+    }
+
+    /**
+     * Define el valor de la propiedad filename.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFilename(String value) {
+        this.filename = value;
     }
 
     /**
