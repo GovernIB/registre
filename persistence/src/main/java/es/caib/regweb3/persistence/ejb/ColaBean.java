ef6cd5a308bd59f8c2798d8663f76b6a9a7ad701
earrivi@dgtic.caib.es <Whutupor0>
2021-06-21 10:23:23 +0200

    Inicio: Nueva caja en el dashboard de Admin entidad de los elementos de la Cola con Error, se muestran solo 7.

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/ColaBean.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/ColaBean.java
@@ -400,6 +400,7 @@ public class ColaBean extends BaseEjbJPA<Cola, Long> implements ColaLocal {
 
         q.setParameter("idEntidad", idEntidad);
         q.setParameter("error", RegwebConstantes.COLA_ESTADO_ERROR);
+        q.setMaxResults(7);
 
         return q.getResultList();
     }
