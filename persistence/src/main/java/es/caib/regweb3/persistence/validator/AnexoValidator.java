dbca09c0691cc13b153253d4362bab511fcfb133
Eduardo Arrivi <earrivi@dgtic.caib.es>
2021-07-07 08:18:14 +0200

    Obligatoriedad del campo validezDocumento

--- persistence/src/main/java/es/caib/regweb3/persistence/validator/AnexoValidator.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/validator/AnexoValidator.java
@@ -13,112 +13,88 @@ import java.util.Arrays;
 /**
  * Created by Fundació BIT.
  * Gestiona las Validaciones del formulario para crear o editar una {@link es.caib.regweb3.model.Anexo}
+ *
  * @author earrivi
  * @author anadal (adaptació sense Spring)
  * Date: 11/02/14
  */
 public class AnexoValidator<T> extends AbstractRegWebValidator<T> {
 
-  protected final Logger log = Logger.getLogger(getClass());
+    protected final Logger log = Logger.getLogger(getClass());
 
 
-  /** Constructor */
-  public AnexoValidator() {
-    super();    
-  }
-  
+    /**
+     * Constructor
+     */
+    public AnexoValidator() {
+        super();
+    }
 
-  
-  public void validate(IValidatorResult<T> errors, T __target__, boolean __isNou__) {
 
-    // Valors Not Null
-    /* __vr.rejectIfEmptyOrWhitespace(__target__,PETICIODEFIRMAID, 
-        "genapp.validation.required",
-        new org.fundaciobit.genapp.common.i18n.I18NArgumentCode(get(PETICIODEFIRMAID)));
-    */   
- 
-    
-    /*
-    if (__isNou__) { // Creació
-      // ================ CREATION
-      // Fitxers
-      if (__vr.getFieldErrorCount(FITXERID) == 0) { // FITXER
-        Object __value = __vr.getFieldValue(__target__, FITXERID);
-        if (__value == null || String.valueOf(__value).trim().length() == 0
-            || String.valueOf(__value).trim().equals("0")) {
-          __vr.rejectValue(FITXERID, "genapp.validation.required",
-              new org.fundaciobit.genapp.common.i18n.I18NArgumentCode(get(FITXERID)));
+    public void validate(IValidatorResult<T> errors, T __target__, boolean __isNou__) {
+
+
+        Anexo anexo = (Anexo) __target__;
+
+
+        if (anexo.getValidezDocumento() == null) {
+            rejectValue(errors, "validezDocumento", "error.valor.requerido"); // , "El camp és obligatori"
         }
-      }
 
-    } else {
-      // ================ UPDATE
-
-    }
-    */
-    
-    Anexo anexo = (Anexo)__target__;
-
-
-      if (anexo.getTipoDocumento() == null || anexo.getTipoDocumento().equals((long) -1)) {
-       rejectValue(errors, "tipoDocumento", "error.valor.requerido"); // , "El camp és obligatori"
-    }
-
-    if(anexo.getTipoDocumental() == null){
-        rejectValue(errors, "tipoDocumental", "error.valor.requerido");
-    }else{
-      if(anexo.getTipoDocumental().getId()== -1){
-          rejectValue(errors, "tipoDocumental", "error.valor.requerido");
-      }
-    }
-
-
-    if(anexo.getOrigenCiudadanoAdmin() == null){
-       rejectValue(errors,"origenCiudadanoAdmin", "error.valor.requerido");
-    }else if(anexo.getOrigenCiudadanoAdmin()!= 0 && anexo.getOrigenCiudadanoAdmin() != 1){
-       rejectValue(errors,"origenCiudadanoAdmin", "error.valor.inesperado.origen");
-    }
-
-
-    rejectIfEmptyOrWhitespace(errors, __target__, "titulo", "error.valor.requerido");
-
-    if(StringUtils.isNotEmpty(anexo.getTitulo())){
-        if(StringUtils.indexOfAny(anexo.getTitulo(), RegwebConstantes.CARACTERES_NO_PERMITIDOS_ARXIU) != -1){
-            rejectValue(errors, "titulo", "error.caracteres.noPermitidos", new I18NArgumentString(Arrays.toString(RegwebConstantes.CARACTERES_NO_PERMITIDOS_ARXIU)));
+        if (anexo.getTipoDocumento() == null || anexo.getTipoDocumento().equals((long) -1)) {
+            rejectValue(errors, "tipoDocumento", "error.valor.requerido"); // , "El camp és obligatori"
         }
-    }
 
-    if(anexo.getTitulo() != null && anexo.getTitulo().length()>200 ){
-       rejectValue(errors, "titulo", "error.valor.maxlenght");
-    }
-
-    
-  } // Final de mètode
-  
-  
-  
-
-  
-
-  
-  @Override
-  public String prefixFieldName() {
-      return "anexo.";
-  }
-
-  @Override
-  public String adjustFieldName(String fieldName) {
-      if (fieldName == null ) {
-        return null;
-      } else {
-        if (fieldName.endsWith(".id")) {
-          return fieldName.substring(0, fieldName.lastIndexOf('.'));
+        if (anexo.getTipoDocumental() == null) {
+            rejectValue(errors, "tipoDocumental", "error.valor.requerido");
+        } else {
+            if (anexo.getTipoDocumental().getId() == -1) {
+                rejectValue(errors, "tipoDocumental", "error.valor.requerido");
+            }
         }
-      }
-      return fieldName;
-  }
-  
-  
+
+
+        if (anexo.getOrigenCiudadanoAdmin() == null) {
+            rejectValue(errors, "origenCiudadanoAdmin", "error.valor.requerido");
+        } else if (anexo.getOrigenCiudadanoAdmin() != 0 && anexo.getOrigenCiudadanoAdmin() != 1) {
+            rejectValue(errors, "origenCiudadanoAdmin", "error.valor.inesperado.origen");
+        }
+
+
+        rejectIfEmptyOrWhitespace(errors, __target__, "titulo", "error.valor.requerido");
+
+        if (StringUtils.isNotEmpty(anexo.getTitulo())) {
+            if (StringUtils.indexOfAny(anexo.getTitulo(), RegwebConstantes.CARACTERES_NO_PERMITIDOS_ARXIU) != -1) {
+                rejectValue(errors, "titulo", "error.caracteres.noPermitidos", new I18NArgumentString(Arrays.toString(RegwebConstantes.CARACTERES_NO_PERMITIDOS_ARXIU)));
+            }
+        }
+
+        if (anexo.getTitulo() != null && anexo.getTitulo().length() > 200) {
+            rejectValue(errors, "titulo", "error.valor.maxlenght");
+        }
+
+
+    } // Final de mètode
+
+
+    @Override
+    public String prefixFieldName() {
+        return "anexo.";
+    }
+
+    @Override
+    public String adjustFieldName(String fieldName) {
+        if (fieldName == null) {
+            return null;
+        } else {
+            if (fieldName.endsWith(".id")) {
+                return fieldName.substring(0, fieldName.lastIndexOf('.'));
+            }
+        }
+        return fieldName;
+    }
+
+
 }
 
 
