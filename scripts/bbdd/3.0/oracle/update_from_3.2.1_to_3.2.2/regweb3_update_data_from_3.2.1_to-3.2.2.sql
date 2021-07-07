c1011702eb2705389fb6782a982cc279accb1041
Eduardo Arrivi <earrivi@dgtic.caib.es>
2021-07-07 08:28:57 +0200

    scripts de datos

--- scripts/bbdd/3.0/oracle/update_from_3.2.1_to_3.2.2/regweb3_update_data_from_3.2.1_to-3.2.2.sql
+++ scripts/bbdd/3.0/oracle/update_from_3.2.1_to_3.2.2/regweb3_update_data_from_3.2.1_to-3.2.2.sql
@@ -1,3 +1,3 @@
 --Actializar campo custodiado
-update RWE_ANEXO set custodiado=true where justificante=true and csv IS NOT NULL;
-update RWE_ANEXO set custodiado=false where justificante=true and csv IS NULL;
+update RWE_ANEXO set custodiado=1 where justificante=1 and csv IS NOT NULL;
+update RWE_ANEXO set custodiado=0 where justificante=1 and csv IS NULL;
