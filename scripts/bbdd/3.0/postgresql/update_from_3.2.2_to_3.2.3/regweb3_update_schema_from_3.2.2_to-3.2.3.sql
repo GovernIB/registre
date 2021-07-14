51d43af65afeca41efb41ecc7f985dcf32810a5e
Eduardo Arrivi <earrivi@dgtic.caib.es>
2021-07-14 13:19:48 +0200

    Anexos confidenciales: Modificar clase Anexo para añadir nuevos campos #484

--- scripts/bbdd/3.0/postgresql/update_from_3.2.2_to_3.2.3/regweb3_update_schema_from_3.2.2_to-3.2.3.sql
+++ scripts/bbdd/3.0/postgresql/update_from_3.2.2_to_3.2.3/regweb3_update_schema_from_3.2.2_to-3.2.3.sql
@@ -1,5 +1,10 @@
 --Nuevo campo en RWE_REGISTRO_DETALLE
 ALTER TABLE RWE_REGISTRO_DETALLE ADD APLICACION_TELEMATICA varchar(255);
 
+--Nuevos campos en RWE_ANEXO
+ALTER TABLE RWE_ANEXO add confidencial bool DEFAULT false not null;
+ALTER TABLE RWE_ANEXO add nombre_fichero varchar(255);
+ALTER TABLE RWE_ANEXO add tamano_fichero int4;
+
 
 
