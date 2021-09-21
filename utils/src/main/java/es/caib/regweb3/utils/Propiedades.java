e9c997598c2183e228d449b8cbd66edb56d107cd
earrivi@dgtic.caib.es <Whutupor0>
2021-09-21 10:23:23 +0200

    Obtener propiedades de los properties de jboss 7.2

---
+++ utils/src/main/java/es/caib/regweb3/utils/Propiedades.java
@@ -0,0 +1,51 @@
+package es.caib.regweb3.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Propiedades {
+
+    @Value("${es.caib.regweb3.sir.serverbase}")
+    private String sirServerbase;
+
+    @Value("${es.caib.regweb3.iscaib}")
+    private Boolean isCaib;
+
+    @Value("${es.caib.regweb3.showtimestamp}")
+    private Boolean showtimestamp;
+
+    @Value("${es.caib.regweb3.defaultlanguage}")
+    private String defaultLanguage;
+
+    @Value("${es.caib.regweb3.preregistre}")
+    private String urlPreregistre;
+
+    @Value("${es.caib.regweb3.archivos.path}")
+    private String archivosPath;
+
+
+    public String getSirServerbase() {
+        return sirServerbase;
+    }
+
+    public Boolean getCaib() {
+        return isCaib;
+    }
+
+    public Boolean getShowtimestamp() {
+        return showtimestamp;
+    }
+
+    public String getDefaultLanguage() {
+        return defaultLanguage;
+    }
+
+    public String getUrlPreregistre() {
+        return urlPreregistre;
+    }
+
+    public String getArchivosPath() {
+        return archivosPath;
+    }
+}
