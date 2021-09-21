656b705a554041d2fdc0e41faf59be4139bc6971
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2021-09-21 14:07:22 +0200

    Informar los metadatos de un anexo a distribución. #500

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/AnexoBean.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/AnexoBean.java
@@ -139,23 +139,23 @@ public class AnexoBean extends BaseEjbJPA<Anexo, Long> implements AnexoLocal {
                 anexoFull.setSignatureCustody(custody.getSignatureInfoOnly(custodyID)); //Firma asociada al anexo
                 anexoFull.setSignatureFileDelete(false);
 
-            //Obtenemos las metadatas de escaneo del anexo si no es justificante
-            if(!anexo.isJustificante()) {
-                List<Metadata> metadataList = new ArrayList<>();
-                //Profundidad color
-                Metadata profundidadColor = custody.getOnlyOneMetadata(custodyID, MetadataConstants.EEMGDE_PROFUNDIDAD_COLOR);
-                if (profundidadColor != null) {
-                    metadataList.add(profundidadColor);
-                }
+                //Obtenemos las metadatas de escaneo del anexo si no es justificante
+                if(!anexo.isJustificante()) {
+                    List<Metadata> metadataList = new ArrayList<>();
+                    //Profundidad color
+                    Metadata profundidadColor = custody.getOnlyOneMetadata(custodyID, MetadataConstants.EEMGDE_PROFUNDIDAD_COLOR);
+                    if (profundidadColor != null) {
+                        metadataList.add(profundidadColor);
+                    }
 
-                //Resolución
-                Metadata resolucion = custody.getOnlyOneMetadata(custodyID, MetadataConstants.EEMGDE_RESOLUCION);
-                if (resolucion != null) {
-                    metadataList.add(resolucion);
-                }
+                    //Resolución
+                    Metadata resolucion = custody.getOnlyOneMetadata(custodyID, MetadataConstants.EEMGDE_RESOLUCION);
+                    if (resolucion != null) {
+                        metadataList.add(resolucion);
+                    }
 
-                anexoFull.setMetadatas(metadataList);
-            }
+                    anexoFull.setMetadatas(metadataList);
+                }
 
             if (log.isDebugEnabled()) {
                 log.debug("SIGNATURE " + custody.getSignatureInfoOnly(custodyID));
@@ -235,6 +235,25 @@ public class AnexoBean extends BaseEjbJPA<Anexo, Long> implements AnexoLocal {
 
                 anexoFull.setSignatureCustody(custody.getSignatureInfo(custodyID));//Firma asociada al anexo
                 anexoFull.setSignatureFileDelete(false);
+
+                //cargamos los metadatos de escaneo
+                if(!anexo.isJustificante()) {
+                    List<Metadata> metadataList = new ArrayList<>();
+
+                    //Profundidad color
+                    Metadata profundidadColor = custody.getOnlyOneMetadata(custodyID, MetadataConstants.EEMGDE_PROFUNDIDAD_COLOR);
+                    if (profundidadColor != null) {
+                        metadataList.add(profundidadColor);
+                    }
+
+                    //Resolución
+                    Metadata resolucion = custody.getOnlyOneMetadata(custodyID, MetadataConstants.EEMGDE_RESOLUCION);
+                    if (resolucion != null) {
+                        metadataList.add(resolucion);
+                    }
+                    anexoFull.setMetadatas(metadataList);
+
+                }
 
             } else if(anexo.getPerfilCustodia().equals(RegwebConstantes.PERFIL_CUSTODIA_ARXIU)){
 
