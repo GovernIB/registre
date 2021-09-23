49a958d9d6ee3e5c9dd48d85018b7659326982f6
Eduardo Arrivi <earrivi@dgtic.caib.es>
2021-09-23 13:49:57 +0200

    Properties: Gestionar las propiedades según los nuevos estándares GOIB #505

--- webapp/src/main/java/es/caib/regweb3/webapp/utils/RegWebSessionLocaleResolver.java
+++ webapp/src/main/java/es/caib/regweb3/webapp/utils/RegWebSessionLocaleResolver.java
@@ -1,11 +1,12 @@
 package es.caib.regweb3.webapp.utils;
 
 import es.caib.regweb3.model.Usuario;
-import es.caib.regweb3.utils.Configuracio;
+import es.caib.regweb3.utils.Propiedades;
 import es.caib.regweb3.utils.RegwebConstantes;
 import es.caib.regweb3.webapp.security.LoginInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -14,51 +15,52 @@ import org.springframework.web.util.WebUtils;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Locale;
+
 /**
- * 
  * @author anadal
- *
  */
 @Component
 public class RegWebSessionLocaleResolver extends SessionLocaleResolver {
 
-  protected final Logger log = LoggerFactory.getLogger(getClass());
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 
-  @Override
-  protected Locale determineDefaultLocale(HttpServletRequest request) {
+    @Autowired
+    private Propiedades propiedades;
 
-    try {
-      HttpSession session = request.getSession();
-      LoginInfo loginInfo = (LoginInfo) session.getAttribute(RegwebConstantes.SESSION_LOGIN_INFO);
+    @Override
+    protected Locale determineDefaultLocale(HttpServletRequest request) {
 
-      String idioma = null;
-      if(loginInfo!=null){
-        Usuario usuario = loginInfo.getUsuarioAutenticado();
-        if (usuario != null && usuario.getIdioma() != null) {
-          idioma = RegwebConstantes.CODIGO_BY_IDIOMA_ID.get(usuario.getIdioma());
+        try {
+            HttpSession session = request.getSession();
+            LoginInfo loginInfo = (LoginInfo) session.getAttribute(RegwebConstantes.SESSION_LOGIN_INFO);
+
+            String idioma = null;
+            if (loginInfo != null) {
+                Usuario usuario = loginInfo.getUsuarioAutenticado();
+                if (usuario != null && usuario.getIdioma() != null) {
+                    idioma = RegwebConstantes.CODIGO_BY_IDIOMA_ID.get(usuario.getIdioma());
+                }
+            }
+
+            if (idioma == null) {
+                idioma = propiedades.getDefaultLanguage();
+            }
+
+            Locale loc = new Locale(idioma);
+            LocaleContextHolder.setLocale(loc);
+            try {
+                this.setLocale(request, null, loc);
+            } catch (Exception e) {
+                WebUtils.setSessionAttribute(request, LOCALE_SESSION_ATTRIBUTE_NAME, loc);
+            }
+            return loc;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return super.determineDefaultLocale(request);
         }
-      }
 
-        
-      if (idioma == null) {
-        idioma = Configuracio.getDefaultLanguage();
-      }
-          
-      Locale loc = new Locale(idioma);
-      LocaleContextHolder.setLocale(loc);
-      try {
-        this.setLocale(request, null, loc);
-      } catch(Exception e) {
-         WebUtils.setSessionAttribute(request, LOCALE_SESSION_ATTRIBUTE_NAME, loc);
-      }
-      return loc;
-    } catch(Exception e) {
-      log.error(e.getMessage(), e);
-      return super.determineDefaultLocale(request);  
+
     }
-    
 
-  }
-  
-  
+
 }
