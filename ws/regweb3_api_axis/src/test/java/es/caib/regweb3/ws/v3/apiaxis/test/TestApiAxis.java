f814e949fef078bea1e6a0f9805b5b884c626c65
Eduardo Arrivi <earrivi@dgtic.caib.es>
2021-09-23 13:50:39 +0200

    Eliminar los servicios de test HelloWorld y HelloWordWithSecurity

--- ws/regweb3_api_axis/src/test/java/es/caib/regweb3/ws/v3/apiaxis/test/TestApiAxis.java
+++ ws/regweb3_api_axis/src/test/java/es/caib/regweb3/ws/v3/apiaxis/test/TestApiAxis.java
@@ -1,7 +1,5 @@
 package es.caib.regweb3.ws.v3.apiaxis.test;
 
-import es.caib.regweb3.ws.v3.apiaxis.RegWebHelloWorldWsServiceLocator;
-import es.caib.regweb3.ws.v3.apiaxis.RegWebHelloWorldWs_PortType;
 
 
 
@@ -21,39 +19,6 @@ public class TestApiAxis {
       String url = "http://ibit151:8080/regweb3/ws/v3/RegWebHelloWorld";
 
       
-      RegWebHelloWorldWsServiceLocator locator = new RegWebHelloWorldWsServiceLocator();
-      locator.setRegWebHelloWorldWsEndpointAddress(url);
-      RegWebHelloWorldWs_PortType service = locator.getRegWebHelloWorldWs();
-      
-      
-      System.out.println("Versio: " +  service.getVersion());
-
-      /*
- String url = "http://ibit151:8080/regweb3/ws/v3/RegWebHelloWorldWithSecurity";
-
-      
- RegWebHelloWorldWithSecurityWsServiceLocator locator = new RegWebHelloWorldWithSecurityWsServiceLocator();
-      locator.setRegWebHelloWorldWithSecurityWsEndpointAddress(url);
-      RegWebHelloWorldWithSecurityWs_PortType service = locator.getRegWebHelloWorldWithSecurityWs();
-      
-      ((Stub) service)._setProperty(Call.USERNAME_PROPERTY, "caibapp");
-      ((Stub) service)._setProperty(Call.PASSWORD_PROPERTY, "caibapp");
-      
-      System.out.println("Versio: " +  service.getVersion());
-      */
-      
-/*
-    String url = "http://localhost:8080/regweb3/ws/v3/RegWebRegistroEntrada";
-
-    
-    RegWebRegistroSalidaWsServiceLocator locator = new RegWebRegistroSalidaWsServiceLocator();
-    locator.setRegWebRegistroSalidaWsEndpointAddress(url);
-    RegWebRegistroSalidaWs_PortType service = locator.getRegWebRegistroSalidaWs();
-
-    // to use Basic HTTP Authentication:
-    ((Stub) service)._setProperty(Call.USERNAME_PROPERTY, "user name");
-    ((Stub) service)._setProperty(Call.PASSWORD_PROPERTY, "password");
-*/
 
     } catch (Exception e) {
       // TODO: handle exception
