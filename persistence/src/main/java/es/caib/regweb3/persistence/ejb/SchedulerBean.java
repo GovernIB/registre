17a6191e6c88390908d8bc42b3dde6ec4f7ad008
earrivi@dgtic.caib.es <Whutupor0>
2021-11-15 12:13:20 +0100

    Cola Custodia: Posibilidad de parar la Cola #521

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/SchedulerBean.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/SchedulerBean.java
@@ -272,7 +272,7 @@ public class SchedulerBean implements SchedulerLocal{
 
             for (Entidad entidad : entidades) {
 
-                if(PropiedadGlobalUtil.pararColaCustodia(entidad.getId())){
+                if(!PropiedadGlobalUtil.pararColaCustodia(entidad.getId())){
 
                     custodiaEjb.custodiarJustificantesEnCola(entidad.getId());
                 }
