a6fa2380717ad4b0debd72971858db567d09fece
Eduardo Arrivi <earrivi@dgtic.caib.es>
2022-01-14 08:56:41 +0100

    Adaptar el nombre JNDi de los Ejb's de la aplicación #504

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/RegistroEntradaConsultaBean.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/RegistroEntradaConsultaBean.java
@@ -15,8 +15,8 @@ import es.caib.regweb3.utils.StringUtils;
 import org.apache.log4j.Logger;
 import org.fundaciobit.genapp.common.i18n.I18NException;
 import org.hibernate.Hibernate;
-import org.jboss.ejb3.annotation.SecurityDomain;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -35,7 +35,7 @@ import static es.caib.regweb3.persistence.ejb.BaseEjbJPA.RESULTADOS_PAGINACION;
  */
 
 @Stateless(name = "RegistroEntradaConsultaEJB")
-@SecurityDomain("seycon")
+@RolesAllowed({"RWE_SUPERADMIN", "RWE_ADMIN", "RWE_USUARI", "RWE_WS_ENTRADA", "RWE_WS_SALIDA", "RWE_WS_CIUDADANO"})
 public class RegistroEntradaConsultaBean implements RegistroEntradaConsultaLocal {
 
     protected final Logger log = Logger.getLogger(getClass());
