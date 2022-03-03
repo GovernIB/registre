c95281e6c35ffa13357a3b2989c8989deba3c3f2
earrivi@dgtic.caib.es <Whutupor0>
2022-03-03 10:11:40 +0100

    BBDD: Mejorar el rendiminento #532

--- webapp/src/main/java/es/caib/regweb3/webapp/validator/CodigoAsuntoValidator.java
+++ webapp/src/main/java/es/caib/regweb3/webapp/validator/CodigoAsuntoValidator.java
@@ -3,14 +3,19 @@ package es.caib.regweb3.webapp.validator;
 import es.caib.regweb3.model.CodigoAsunto;
 import es.caib.regweb3.persistence.ejb.CodigoAsuntoLocal;
 import es.caib.regweb3.utils.RegwebConstantes;
+import es.caib.regweb3.webapp.security.LoginInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.ejb.EJB;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Fundació BIT.
@@ -40,22 +45,29 @@ public class CodigoAsuntoValidator implements Validator {
 
         
         for (Long idioma :  RegwebConstantes.IDIOMAS_UI) {
-          ValidationUtils.rejectIfEmpty(errors, "traducciones[" + RegwebConstantes.CODIGO_BY_IDIOMA_ID.get(idioma) + "].nombre", "error.valor.requerido", "El camp nom és obligatori");
+            ValidationUtils.rejectIfEmpty(errors, "traducciones[" + RegwebConstantes.CODIGO_BY_IDIOMA_ID.get(idioma) + "].nombre", "error.valor.requerido", "El camp nom és obligatori");
         }
 
         // Identificador único
         try {
+            HttpServletRequest request =
+                    ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+            HttpSession session = request.getSession();
+            LoginInfo loginInfo = (LoginInfo) session.getAttribute(RegwebConstantes.SESSION_LOGIN_INFO);
+
+
             if (codigoAsunto.getCodigo() != null && codigoAsunto.getCodigo().length() > 0) {
 
                 if (codigoAsunto.getId() != null) {  // Se trata de una modificación
 
-                    if (codigoAsuntoEjb.existeCodigoEdit(codigoAsunto.getCodigo(), codigoAsunto.getId(),codigoAsunto.getEntidad().getId())) {
+                    if (codigoAsuntoEjb.existeCodigoEdit(codigoAsunto.getCodigo(), codigoAsunto.getId(), loginInfo.getEntidadActiva().getId())) {
                         errors.rejectValue("codigo", "error.identificador.existe", "L'identificador ja existeix");
                     }
 
                 } else {
 
-                    if (codigoAsuntoEjb.findByCodigoEntidad(codigoAsunto.getCodigo(),codigoAsunto.getEntidad().getId()) != null) {
+                    if (codigoAsuntoEjb.findByCodigoEntidad(codigoAsunto.getCodigo(), loginInfo.getEntidadActiva().getId()) != null) {
                         errors.rejectValue("codigo", "error.identificador.existe", "L'identificador ja existeix");
                     }
                 }
@@ -64,8 +76,5 @@ public class CodigoAsuntoValidator implements Validator {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
-
 }
