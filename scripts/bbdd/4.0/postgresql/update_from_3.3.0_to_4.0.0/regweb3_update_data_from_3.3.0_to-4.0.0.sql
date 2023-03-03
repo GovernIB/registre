f3d45f8be6e5a3461c8952bae6c2bbf217f7512a
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2023-03-03 09:19:05 +0100

    Script per actualitzar la bd amb els canvis de model de SICRES 4 #538

--- scripts/bbdd/4.0/postgresql/update_from_3.3.0_to_4.0.0/regweb3_update_data_from_3.3.0_to-4.0.0.sql
+++ scripts/bbdd/4.0/postgresql/update_from_3.3.0_to_4.0.0/regweb3_update_data_from_3.3.0_to-4.0.0.sql
@@ -3,4 +3,9 @@ update RWE_INTERESADO SET RECEPNOTIF= 0 where RECEPNOTIF is null;
 update RWE_INTERESADO SET AVISONOTIFEMAIL= 0 where AVISONOTIFEMAIL is null;
 update RWE_INTERESADO SET AVISONOTIFSMS= 0 where AVISONOTIFSMS is null;
 
+--Actualitzar a un valor por defecto los campos que son null
+update RWE_PERSONA SET RECEPNOTIF= 0 where RECEPNOTIF is null;
+update RWE_PERSONA SET AVISONOTIFEMAIL= 0 where AVISONOTIFEMAIL is null;
+update RWE_PERSONA SET AVISONOTIFSMS= 0 where AVISONOTIFSMS is null;
+
 
