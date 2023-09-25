5c49cfb6395a55c989d3a5bc1162b172f0ffa086
Eduardo Arrivi <earrivi@dgtic.caib.es>
2023-09-25 09:23:50 +0200

    SIR: Aceptar el tipo de documento .csv en los intercambios #616

--- utils/src/main/java/es/caib/regweb3/utils/RegwebConstantes.java
+++ utils/src/main/java/es/caib/regweb3/utils/RegwebConstantes.java
@@ -606,7 +606,8 @@ public interface RegwebConstantes {
     int ANEXO_IDENTIFICADOR_MAXLENGTH_SIR = 50;
 
     /* ---------------ANEXO LIMITACIONES SIR ----------*/
-    String[] ANEXO_EXTENSIONES_SIR = new String[]{"jpg", "jpeg", "odt", "odp", "ods", "odg", "docx", "xlsx", "pptx", "pdf", "png", "rtf", "svg", "tiff", "txt", "xml", "csig", "xsig"};
+    String[] ANEXO_EXTENSIONES_SIR = new String[]{"jpg", "jpeg", "odt", "odp", "ods", "odg", "docx", "xlsx", "pptx", "pdf", "png", "rtf", "svg", "tiff", "txt", "xml", "csig", "xsig","csv"};
+
     int    ANEXO_NUMEROMAX_SIR = 5;
     Long   ANEXO_TAMANOMAX_SIR = 10485760L;
     Long   ANEXO_TAMANOMAXTOTAL_SIR = 15728640L;
