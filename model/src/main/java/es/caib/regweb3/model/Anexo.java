8a8133cc4f3678195a41a0accf6162ee3896d61a
Eduardo Arrivi <earrivi@dgtic.caib.es>
2024-01-29 09:06:41 +0100

    Nuevo índice en el campo CUSTODIAID

--- model/src/main/java/es/caib/regweb3/model/Anexo.java
+++ model/src/main/java/es/caib/regweb3/model/Anexo.java
@@ -24,6 +24,7 @@ import java.util.List;
         indexes = {
                 @Index(name = "RWE_ANEXO_TDOCAL_FK_I", columnList = "TDOCUMENTAL"),
                 @Index(name = "RWE_ANEXO_REGDET_FK_I", columnList = "REGISTRODETALLE"),
+                @Index(name = "RWE_ANEXO_CUSTID_FK_I", columnList = "CUSTODIAID")
         })
 @XmlRootElement(name = "anexo")
 @XmlAccessorType(XmlAccessType.FIELD)
