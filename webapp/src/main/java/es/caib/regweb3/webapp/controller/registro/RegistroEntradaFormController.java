29dad3f77a95e9d5a2a81fec32cbadbf484a51a9
Eduardo Arrivi <earrivi@dgtic.caib.es>
2024-02-13 08:47:49 +0100

    Gestionar que Entidad puede hacer registros de salida a Personas físicas y jurídicas #632

--- webapp/src/main/java/es/caib/regweb3/webapp/controller/registro/RegistroEntradaFormController.java
+++ webapp/src/main/java/es/caib/regweb3/webapp/controller/registro/RegistroEntradaFormController.java
@@ -115,6 +115,7 @@ public class RegistroEntradaFormController extends AbstractRegistroCommonFormCon
         registroEntrada.setOficina(oficina);
         registroEntrada.setLibro(getLibroEntidad(request));
         registroEntrada.getRegistroDetalle().setPresencial(true);
+        registroEntrada.getRegistroDetalle().setTipoDocumentacionFisica(RegwebConstantes.TIPO_DOCFISICA_NO_ACOMPANYA_DOC);
 
         //Eliminamos los posibles interesados de la Sesion
         eliminarVariableSesion(request, RegwebConstantes.SESSION_INTERESADOS_ENTRADA);
