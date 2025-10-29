ce9d963ffa547bad40b2e7f6302f9ae2f59dc8e0
earrivi <earrivi@portsdebalears.com>
2025-10-29 10:00:04 +0100

    idSesion: Se modifica el tiempo de purgado de las sesiones a 10 días, modificable por propiedad

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/SesionBean.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/SesionBean.java
@@ -184,9 +184,9 @@ public class SesionBean extends BaseEjbJPA<Sesion, Long> implements SesionLocal{
     private void purgarSesionesIniciadas(Long idEntidad) throws Exception{
 
         Calendar hoy = Calendar.getInstance(); //obtiene la fecha de hoy
-        hoy.add(Calendar.MINUTE, -PropiedadGlobalUtil.getSesionMinutosPurgadoIniciadas(idEntidad)); //el -X indica que se le restaran X minutos
+        hoy.add(Calendar.DATE, -PropiedadGlobalUtil.getSesionDiasPurgado(idEntidad)); //el -X indica que se le restaran X días
 
-        List<?> result =  em.createQuery("select distinct(s.id) from Sesion as s where s.usuario.entidad.id = :idEntidad and s.estado = :iniciada and s.fecha <= :fecha")
+        List<?> result = em.createQuery("select distinct(s.id) from Sesion as s where s.usuario.entidad.id = :idEntidad and s.estado = :iniciada and s.fecha <= :fecha")
                 .setParameter("idEntidad", idEntidad)
                 .setParameter("iniciada", RegwebConstantes.SESION_INICIADA)
                 .setParameter("fecha", hoy.getTime()).getResultList();
@@ -198,9 +198,9 @@ public class SesionBean extends BaseEjbJPA<Sesion, Long> implements SesionLocal{
     private void purgarSesionesNoIniciadas(Long idEntidad) throws Exception{
 
         Calendar hoy = Calendar.getInstance(); //obtiene la fecha de hoy
-        hoy.add(Calendar.MINUTE, -PropiedadGlobalUtil.getSesionMinutosPurgadoNoIniciadas(idEntidad)); //el -X indica que se le restaran X minutos
+        hoy.add(Calendar.DATE, -PropiedadGlobalUtil.getSesionDiasPurgado(idEntidad)); //el -X indica que se le restaran X días
 
-        List<?> result =  em.createQuery("select distinct(s.id) from Sesion as s where s.usuario.entidad.id = :idEntidad and s.estado = :no_iniciada and s.fecha <= :fecha")
+        List<?> result = em.createQuery("select distinct(s.id) from Sesion as s where s.usuario.entidad.id = :idEntidad and s.estado = :no_iniciada and s.fecha <= :fecha")
                 .setParameter("idEntidad", idEntidad)
                 .setParameter("no_iniciada", RegwebConstantes.SESION_NO_INICIADA)
                 .setParameter("fecha", hoy.getTime()).getResultList();
@@ -211,9 +211,9 @@ public class SesionBean extends BaseEjbJPA<Sesion, Long> implements SesionLocal{
     private void purgarSesionesErrorFinalidadas(Long idEntidad) throws Exception{
 
         Calendar hoy = Calendar.getInstance(); //obtiene la fecha de hoy
-        hoy.add(Calendar.MINUTE, -PropiedadGlobalUtil.getSesionMinutosPurgadoFinalizadas(idEntidad)); //el -X indica que se le restaran X minutos
+        hoy.add(Calendar.DATE, -PropiedadGlobalUtil.getSesionDiasPurgado(idEntidad)); //el -X indica que se le restaran X días
 
-        List<?> result =  em.createQuery("select distinct(s.id) from Sesion as s where s.usuario.entidad.id = :idEntidad and (s.estado = :error or s.estado = :finalizada) and s.fecha <= :fecha")
+        List<?> result = em.createQuery("select distinct(s.id) from Sesion as s where s.usuario.entidad.id = :idEntidad and (s.estado = :error or s.estado = :finalizada) and s.fecha <= :fecha")
                 .setParameter("idEntidad", idEntidad)
                 .setParameter("error", RegwebConstantes.SESION_ERROR)
                 .setParameter("finalizada", RegwebConstantes.SESION_FINALIZADA)
