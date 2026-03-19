61a3452bff64727e99b84389f88e9c055fac3e1c
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2026-03-19 13:38:13 +0100

    MANT/REGWEB Poposta Millora control integracions escanners (DIGITALIB) #732

--- webapp/src/main/java/es/caib/regweb3/webapp/controller/registro/AnexoScanController.java
+++ webapp/src/main/java/es/caib/regweb3/webapp/controller/registro/AnexoScanController.java
@@ -254,7 +254,7 @@ public class AnexoScanController extends AnexoController {
         }
 
         // recuperamos la integración que queremos actualizar su estado
-        Long idIntegracion = Long.valueOf(request.getSession().getAttribute("integracionId").toString());
+        Long idIntegracion = Long.valueOf(request.getSession().getAttribute("idIntegracion").toString());
 
         try {
 
