27de4645ca344ba72139cfd6223a5f77b1648fa5
mgonzalez <mmgonzalez@ibdigital.caib.es>
2026-05-08 11:09:53 +0200

    Optimització de la query de cerca d'interessats per DNI #740

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/PersonaBean.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/PersonaBean.java
@@ -420,15 +420,16 @@ public class PersonaBean extends BaseEjbJPA<Persona, Long> implements PersonaLoc
 
         if (tipoPersona.equals(RegwebConstantes.TIPO_PERSONA_FISICA)) {
             queryBase = "Select persona.id, persona.nombre, persona.apellido1, persona.apellido2, persona.documento from Persona as persona ";
-            where.add(DataBaseUtils.like("persona.documento", "text", parametros, text));
+            where.add(" persona.documento like upper(:text) ");
+            parametros.put("text","%" + text +"%");
             //where.add(DataBaseUtils.like("CONCAT(persona.nombre,' ',persona.apellido1,' ',persona.apellido2,' - ', persona.documento)", "text", parametros, text));
             where.add(" persona.tipo = :tipoPersona ");
             parametros.put("tipoPersona", RegwebConstantes.TIPO_PERSONA_FISICA);
 
         } else if (tipoPersona.equals(RegwebConstantes.TIPO_PERSONA_JURIDICA)) {
             queryBase = "Select persona.id, persona.razonSocial, persona.documento from Persona as persona ";
-            where.add(DataBaseUtils.like("persona.documento", "text", parametros, text));
-
+            where.add(" persona.documento like upper(:text) ");
+            parametros.put("text","%" + text +"%");
             //where.add(DataBaseUtils.like("CONCAT(persona.razonSocial,' - ', persona.documento)", "text", parametros, text));
             where.add(" persona.tipo = :tipoPersona ");
             parametros.put("tipoPersona", RegwebConstantes.TIPO_PERSONA_JURIDICA);
