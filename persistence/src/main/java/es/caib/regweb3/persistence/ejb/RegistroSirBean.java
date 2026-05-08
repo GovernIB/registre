1d91938d8acaab6f32f6e694b4987728918d4c4b
mgonzalez <mmgonzalez@ibdigital.caib.es>
2026-05-08 10:50:18 +0200

    Error distribuint registres des de la COA sense organisme destí #739

--- persistence/src/main/java/es/caib/regweb3/persistence/ejb/RegistroSirBean.java
+++ persistence/src/main/java/es/caib/regweb3/persistence/ejb/RegistroSirBean.java
@@ -1582,7 +1582,11 @@ public class RegistroSirBean extends BaseEjbJPA<RegistroSir, Long> implements Re
         // Obtenemos el Organismo destino indicado
         Organismo organismoDestino = organismoEjb.findByIdLigero(idOrganismoDestino);
 
-        registroEntrada.setDestino(organismoDestino);
+        if(organismoDestino == null){
+            registroEntrada.setDestino(oficinaActiva.getOrganismoResponsable());
+        }else {
+            registroEntrada.setDestino(organismoDestino);
+        }
         registroEntrada.setDestinoExternoCodigo(null);
         registroEntrada.setDestinoExternoDenominacion(null);
 
